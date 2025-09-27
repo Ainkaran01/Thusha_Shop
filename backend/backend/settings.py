1b1081f34fa8b0a3d04baaae31733f1bfd05c7ff
Ainkaran01 <kirubakaranainkaran@gmail.com>
2025-09-27 13:56:58 +0530

    Virtual try code

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,8 +37,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:8081",
     "http://localhost:8080",
+    "http://127.0.0.1:3000",
 ]
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 CORS_ALLOW_METHODS = [
