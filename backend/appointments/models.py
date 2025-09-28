9d59152738d7bbc0c4a43468b7d0fd6c32dee2c8
Nadesalingam Vishagan <166485262+Vishagan-CST@users.noreply.github.com>
2025-09-28 14:54:07 +0530

    Update models.py

--- backend/appointments/models.py
+++ backend/appointments/models.py
@@ -1,4 +1,4 @@
- from django.db import models
+from django.db import models
 from django.contrib.auth import get_user_model
 from django.dispatch import receiver
 from doctors.models import DoctorProfile
